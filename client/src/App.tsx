6580f55a9896a8ef89a435390434c5935a4ef4fb
Bambo <kyrstke@gmail.com>
2023-06-22 15:39:11 +0200

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { TodoList } from './components/TodoList';
+import { TodoForm } from './components/TodoForm';
+
+
+function App() {
+    return (
+        <>
+            <TodoList />
+            {/* <TodoForm /> */}
+        </>
+    );
+}
+
+export default App;
